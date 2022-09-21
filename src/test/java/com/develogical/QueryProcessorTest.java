5e4411afa63605e3be8f766e9c0add41b5e28155
Alexander <monkeyjerr@DESKTOP-U8NG0OG.localdomain>
2022-09-21 02:17:31 -0400

    got the query to work

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -30,4 +30,9 @@ public class QueryProcessorTest {
         assertThat(queryProcessor.process("what is your name"), containsString("MyTeam"));
     }
 
+    @Test
+    public void knowsUrMom() throws Exception {
+        assertThat(queryProcessor.process("ur mom"), containsString("ur sister"));
+    }
+
 }
